68596890c8ef92c2d92a8166468dbbb6c8ed72f0
Val <valeria.lukashevich@gmail.com>
2022-12-29 16:19:53 +0200

    final commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/login">Log in</router-link> |
+      <router-link to="/login">Log in</router-link>
 
     </nav>
     <router-view/>
